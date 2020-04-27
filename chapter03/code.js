a19ccdfc405b5494d2027156689ad34375ca4f03
changxianglin <281532612@qq.com>
2020-04-27 23:56:14 +0800

    function

--- chapter03/code.js
+++ chapter03/code.js
@@ -56,21 +56,49 @@
 //
 // wieldSword.swordType = 'katana'
 
-var store = {
-    nextId: 1,
-    cache: {},
-    add: function (fn) {
-        if (!fn.id) {
-            fn.id = this.nextId++
-            this.cache[fn.id] = fn
-            return true
-        }
-    }
-}
+// var store = {
+//     nextId: 1,
+//     cache: {},
+//     add: function (fn) {
+//         if (!fn.id) {
+//             fn.id = this.nextId++
+//             this.cache[fn.id] = fn
+//             return true
+//         }
+//     }
+// }
+//
+// function ninja() {
+//     console.log(store.add(ninja))
+//     console.log(!store.add(ninja))
+// }
+//
+// ninja()
 
-function ninja() {
-    console.log(store.add(ninja))
-    console.log(!store.add(ninja))
-}
-
-ninja()
+// function isPrime(value) {
+//     if (!isPrime.answers) {
+//         isPrime.answers = {}
+//     }
+//     if (isPrime.answers[value] !== 'undefined') {
+//         return isPrime.answers[value]
+//     }
+//     var prime = value !== 0 && value !== 1;
+//     for (var i = 2; i < value; i++) {
+//         if (value % i === 0) {
+//             prime = false
+//             break
+//         }
+//     }
+//     return isPrime.answers[value] = prime
+// }
+//
+// console.log(isPrime(5), '5 is primse!')
+// console.log(isPrime.answers[5], 'The answer was cached!')
+//
+// if (!isPrime.answers) {
+//     isPrime.answers = {}
+// }
+//
+// if (isPrime.answers['value'] !== 'undefined') {
+//     return isPrime.answers['value']
+// }
