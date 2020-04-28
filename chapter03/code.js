ffa40ecd04de41583c892e476b7e599d730f377d
changxianglin <281532612@qq.com>
2020-04-28 23:59:37 +0800

    arrow function

--- chapter03/code.js
+++ chapter03/code.js
@@ -151,3 +151,11 @@
 // (function (a) {
 //     console.log(a)
 // })(3)
+
+// var values = [0, 3, 2, 5, 7, 4, 8, 1]
+// values.sort(function (value1, value2) {
+//     return value1 - value2
+// })
+//
+// values.sort((values1, values2) => values1 - values2)
+
