7e6df5abfb15a48f9380513e92f3911bd15b9850
changxianglin <281532612@qq.com>
2020-05-03 23:57:27 +0800

    alias

--- chapter04/code.js
+++ chapter04/code.js
@@ -1,5 +1,6 @@
 function whatever(a, b, c) {
     console.log(a === 1, 'The value of a is 1')
     console.log(b === 2, 'The value of b is 2')
-    console.log(c )
+    console.log(c === 3, 'The value of c is 3')
 }
+
