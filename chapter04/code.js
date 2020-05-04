fca404c4c92962d16d06bd7cf215dd0c129f2461
changxianglin <281532612@qq.com>
2020-05-04 23:58:36 +0800

    use func

--- chapter04/code.js
+++ chapter04/code.js
@@ -1,6 +1,47 @@
-function whatever(a, b, c) {
-    console.log(a === 1, 'The value of a is 1')
-    console.log(b === 2, 'The value of b is 2')
-    console.log(c === 3, 'The value of c is 3')
+// function whatever(a, b, c) {
+//     console.log(a === 1, 'The value of a is 1')
+//     console.log(b === 2, 'The value of b is 2')
+//     console.log(c === 3, 'The value of c is 3')
+// }
+
+
+// function skulk(name) {
+//     console.log(name)
+// }
+//
+// function Ninja(name) {
+//     console.log(name)
+// }
+//
+// skulk('Hattori')
+//
+// (function (who) {
+//     return who
+// })('Hattori')
+//
+// var ninja = {
+//     skulk: function () {
+//
+//     }
+// }
+//
+// ninja.skulk('Hattori')
+// ninja = new Ninja('Hattori')
+// skulk.call(ninja, 'Hattori')
+// skulk.apply(ninja, ['Hattori'])
+
+function ninja() {
+
 }
 
+ninja()
+
+var samurai = function () {
+
+}
+
+samurai()
+
+(function () {
+    console.log('EFIE')
+})()
