c100887dd8e15dd7ba9e796fbcd22d28f69dc22e
changxianglin <281532612@qq.com>
2020-05-11 22:22:45 +0800

    self arguments

--- chapter05/code.js
+++ chapter05/code.js
@@ -7,20 +7,40 @@ const log = console.log.bind(console)
 //
 // outerFunction()
 
-var outerValue = 'samurai'
-var later
+// var outerValue = 'samurai'
+// var later
+//
+// function outerFunction() {
+//     var innerValue = 'ninja'
+//
+//     function innerFunction() {
+//         log(outerValue === 'samurai')
+//
+//         log(innerValue === 'ninja')
+//     }
+//
+//     later = innerFunction
+// }
+//
+// outerFunction()
+// later()
 
-function outerFunction() {
-    var innerValue = 'ninja'
-
-    function innerFunction() {
-        log(outerValue === 'samurai')
-
-        log(innerValue === 'ninja')
+function Ninja() {
+    var feints = 0
+    this.getFeints = function () {
+        return feints
     }
-
-    later = innerFunction
+    this.feint = function () {
+        feints++
+    }
 }
 
-outerFunction()
-later()
+var ninja1 = new Ninja()
+ninja1.feint()
+
+log(ninja1.feints === undefined)
+
+log(ninja1.getFeints() === 1)
+
+var ninja2 = new Ninja()
+log(ninja2.getFeints() === 0)
