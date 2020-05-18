7f9694c7062f0f6acc95a9c3d4b778d76c7d8514
changxianglin <281532612@qq.com>
2020-05-19 00:18:12 +0800

    iterator and next

--- chapter06/code.js
+++ chapter06/code.js
@@ -1,11 +1,28 @@
 const log = console.log.bind(console)
 
+// function* WeaponGenerator() {
+//     yield 'Katana'
+//     yield 'Wakizashi'
+//     yield 'Kusarigama'
+// }
+//
+// for (let weapon of WeaponGenerator()) {
+//     log(weapon !== undefined)
+// }
+
 function* WeaponGenerator() {
     yield 'Katana'
     yield 'Wakizashi'
-    yield 'Kusarigama'
 }
 
-for (let weapon of WeaponGenerator()) {
-    log(weapon !== undefined)
-}
+const weaponsIterator = WeaponGenerator()
+
+const result1 = weaponsIterator.next()
+
+log(typeof result1 === 'object' && result1.value === 'Katana' && !result1.done, 'Katana received!')
+
+const result2 = weaponsIterator.next()
+log(typeof result2 === 'object' && result2.value === 'Wakizashi' && !result2.done, 'Wakizashi received!')
+
+const result3 = weaponsIterator.next()
+log(typeof result3 === 'object' && result3.value === undefined && result3.done, 'There are no more results!')
