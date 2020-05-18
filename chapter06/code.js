f931ca1138a8e37521f63c78992d3b47a36f0c4f
changxianglin <281532612@qq.com>
2020-05-19 00:09:20 +0800

    generator and iterator

--- chapter06/code.js
+++ chapter06/code.js
@@ -1,2 +1,11 @@
 const log = console.log.bind(console)
 
+function* WeaponGenerator() {
+    yield 'Katana'
+    yield 'Wakizashi'
+    yield 'Kusarigama'
+}
+
+for (let weapon of WeaponGenerator()) {
+    log(weapon !== undefined)
+}
