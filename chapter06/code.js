83c6e965ae1e5a8133bca46bfd189a0370e197a5
changxianglin <281532612@qq.com>
2020-05-19 00:30:55 +0800

    iterator and while and for-of

--- chapter06/code.js
+++ chapter06/code.js
@@ -10,6 +10,23 @@ const log = console.log.bind(console)
 //     log(weapon !== undefined)
 // }
 
+// function* WeaponGenerator() {
+//     yield 'Katana'
+//     yield 'Wakizashi'
+// }
+//
+// const weaponsIterator = WeaponGenerator()
+//
+// const result1 = weaponsIterator.next()
+//
+// log(typeof result1 === 'object' && result1.value === 'Katana' && !result1.done, 'Katana received!')
+//
+// const result2 = weaponsIterator.next()
+// log(typeof result2 === 'object' && result2.value === 'Wakizashi' && !result2.done, 'Wakizashi received!')
+//
+// const result3 = weaponsIterator.next()
+// log(typeof result3 === 'object' && result3.value === undefined && result3.done, 'There are no more results!')
+
 function* WeaponGenerator() {
     yield 'Katana'
     yield 'Wakizashi'
@@ -17,12 +34,11 @@ function* WeaponGenerator() {
 
 const weaponsIterator = WeaponGenerator()
 
-const result1 = weaponsIterator.next()
+// let item
+// while (!(item = weaponsIterator.next()).done) {
+//     log(item !== null, item.value)
+// }
 
-log(typeof result1 === 'object' && result1.value === 'Katana' && !result1.done, 'Katana received!')
-
-const result2 = weaponsIterator.next()
-log(typeof result2 === 'object' && result2.value === 'Wakizashi' && !result2.done, 'Wakizashi received!')
-
-const result3 = weaponsIterator.next()
-log(typeof result3 === 'object' && result3.value === undefined && result3.done, 'There are no more results!')
+for (let item of WeaponGenerator ()) {
+    log(item !== null, item)
+}
