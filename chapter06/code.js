ef6e72dc6b32212a4254a676b8c8144f576ed801
changxianglin <281532612@qq.com>
2020-05-19 23:59:21 +0800

    next

--- chapter06/code.js
+++ chapter06/code.js
@@ -27,18 +27,23 @@ const log = console.log.bind(console)
 // const result3 = weaponsIterator.next()
 // log(typeof result3 === 'object' && result3.value === undefined && result3.done, 'There are no more results!')
 
-function* WeaponGenerator() {
-    yield 'Katana'
-    yield 'Wakizashi'
-}
-
-const weaponsIterator = WeaponGenerator()
+// function* WeaponGenerator() {
+//     yield 'Katana'
+//     yield 'Wakizashi'
+// }
+//
+// const weaponsIterator = WeaponGenerator()
 
 // let item
 // while (!(item = weaponsIterator.next()).done) {
 //     log(item !== null, item.value)
 // }
 
-for (let item of WeaponGenerator ()) {
-    log(item !== null, item)
+// for (let item of WeaponGenerator ()) {
+//     log(item !== null, item)
+// }
+
+function* WarriorGenerator() {
+    yield 'Sun Tzu'
+    yield* NinjaGenerator()
 }
