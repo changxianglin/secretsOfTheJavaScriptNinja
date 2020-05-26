047ee92208f5418c3e74bd999bca2127041bddc5
changxianglin <281532612@qq.com>
2020-05-26 22:55:33 +0800

    send ajax use promise

--- chapter06/code.js
+++ chapter06/code.js
@@ -157,3 +157,29 @@ const log = console.log.bind(console)
 // promise.then(
 //     () => log('Happy path , were not be called!')
 // ).catch(err => log('A promise was explicitly rejected!'))
+
+function getJSON(url) {
+    return new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest()
+        request.open('GET', url)
+        request.onload = function () {
+            try {
+                if (this.status === 200) {
+                    resolve(JSON.parse(this.response))
+                } else {
+                    reject(this.status + " " + this.statusText)
+                }
+            } catch (e) {
+                reject(e.message)
+            }
+        }
+        request.onerror = function () {
+            reject(this.status + ' ' + this.statusText)
+        }
+        request.send()
+    })
+}
+
+getJSON('data/ninjas.json').then(ninjas => {
+    log(ninjas !== null, 'Ninjas obtained!')
+}).catch(e => log('Should not be here: ' + e))
