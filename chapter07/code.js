8ad5162a4e6b6775cd4407d24affff1cec6c3b17
changxianglin <281532612@qq.com>
2020-05-28 23:50:45 +0800

    setprototypeOf

--- chapter07/code.js
+++ chapter07/code.js
@@ -1,21 +1,34 @@
 var log = console.log.bind(console)
 
-let obj = {
-    prop1: 1,
-    prop2: function () {
+// let obj = {
+//     prop1: 1,
+//     prop2: function () {
+//
+//     },
+//     prop3: {},
+// }
+//
+// obj.prop1 = 1
+// obj.prop1 = []
+// delete obj.prop2
+//
+// obj.prop4 = 'Hello'
+//
+// {
+//     prop1: [],
+//     prop3: {},
+//     prop4: 'Hello'
+// }
 
-    },
-    prop3: {},
-}
+const yoshi = { skulk: true }
+const hattori = { sneak: true }
+const kuma = { creep: true }
 
-obj.prop1 = 1
-obj.prop1 = []
-delete obj.prop2
+log('skulk' in yoshi, 'Yoshi can skulk')
+log(!("sneak" in yoshi), 'Yoshi cannot sneak')
+log(!('creep' in yoshi), 'Yoshi cannot creep')
 
-obj.prop4 = 'Hello'
+Object.setPrototypeOf(yoshi, hattori)
 
-{
-    prop1: [],
-    prop3: {},
-    prop4: 'Hello'
-}
+log('sneak' in yoshi, 'Yoshi can now sneak')
+log(!("creep" in hattori), 'Hattori cannot creep')
