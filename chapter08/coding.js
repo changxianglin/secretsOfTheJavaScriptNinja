00e0c027d619614aa5ec84e1c066bdbfec5c80a9
changxianglin <281532612@qq.com>
2020-06-18 23:46:00 +0800

    use es6

--- chapter08/coding.js
+++ chapter08/coding.js
@@ -8,17 +8,39 @@ const log = console.log.bind(console)
 //
 // ninja.skillLevel = 20
 
-const ninjaCollection = {
-    ninjas: ['Yoshi', 'Kuma', 'Hattori'],
+// const ninjaCollection = {
+//     ninjas: ['Yoshi', 'Kuma', 'Hattori'],
+//     get firstNinja() {
+//         log('Getting firstNinja')
+//         return this.ninjas[0]
+//     },
+//     set firstNinja(value) {
+//         log('Setting firstNinja')
+//         this.ninjas[0] = value
+//     }
+// }
+//
+// log(ninjaCollection.firstNinja === 'Yoshi', 'Yoshi is the first ninja')
+//
+// ninjaCollection.firstNinja = 'Hachi'
+//
+// log(ninjaCollection.firstNinja === 'Hachi' && ninjaCollection.ninjas[0] === 'Hachi', 'Now Hachi is the first ninja')
+
+class NinjaCollection {
+    constructor() {
+        this.ninjas = ["Yoshi", 'Kuma', 'Hattori']
+    }
     get firstNinja() {
         log('Getting firstNinja')
         return this.ninjas[0]
-    },
+    }
     set firstNinja(value) {
         log('Setting firstNinja')
         this.ninjas[0] = value
     }
 }
+
+const ninjaCollection = new NinjaCollection()
 
 log(ninjaCollection.firstNinja === 'Yoshi', 'Yoshi is the first ninja')
 
