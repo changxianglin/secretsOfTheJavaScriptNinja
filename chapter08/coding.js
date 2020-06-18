e130afc2e5a257643422cbade4144ef62d6ae911
changxianglin <281532612@qq.com>
2020-06-18 23:40:12 +0800

    use get and set change

--- chapter08/coding.js
+++ chapter08/coding.js
@@ -1,2 +1,27 @@
 const log = console.log.bind(console)
 
+// function Ninja(level) {
+//     this.skillLevel = level
+// }
+//
+// const ninja = new Ninja(100)
+//
+// ninja.skillLevel = 20
+
+const ninjaCollection = {
+    ninjas: ['Yoshi', 'Kuma', 'Hattori'],
+    get firstNinja() {
+        log('Getting firstNinja')
+        return this.ninjas[0]
+    },
+    set firstNinja(value) {
+        log('Setting firstNinja')
+        this.ninjas[0] = value
+    }
+}
+
+log(ninjaCollection.firstNinja === 'Yoshi', 'Yoshi is the first ninja')
+
+ninjaCollection.firstNinja = 'Hachi'
+
+log(ninjaCollection.firstNinja === 'Hachi' && ninjaCollection.ninjas[0] === 'Hachi', 'Now Hachi is the first ninja')
--- chapter08/controlObject.js
+++ chapter08/controlObject.js
@@ -2,4 +2,6 @@
 * 第 8 章 控制对象的访问
 *
 * 8.1 使用 getter 与 setter 控制属性访问
+*
+* 8.1.1 定义 setter 和 getter
 * */
