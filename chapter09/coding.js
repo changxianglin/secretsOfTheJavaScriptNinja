25219de14469fee4cbe4399ce0225ff9750dd86a
changxianglin <281532612@qq.com>
2020-06-25 23:46:01 +0800

    crete array

--- chapter09/coding.js
+++ chapter09/coding.js
@@ -1,20 +1,22 @@
 const log = console.log.bind(console)
 
-const ninjas = ['Kuma', 'Hattori', 'Yagyu']
-const samurai = new Array('Oda', 'Tomoe')
+// const ninjas = ['Kuma', 'Hattori', 'Yagyu']
+// const samurai = new Array('Oda', 'Tomoe')
+//
+// log(ninjas.length === 3, 'There are three ninjas')
+// log(samurai.length === 2, 'And only two samurai')
+//
+// log(ninjas[0] === 'Kuma', 'Kuma is the first ninja')
+// log(samurai[samurai.length - 1] === 'Tomoe', 'Tomoe is the last samurai')
+//
+// log(ninjas[4] === undefined, 'We get undefined if we try to access an out of bounds index')
+//
+// ninjas[4] = 'Ishi'
+// log(ninjas.length === 5, 'Arrays are automatically expanded')
+//
+// ninjas.length = 2
+// log(ninjas.length === 2, 'There are only two ninjas now')
+// log(ninjas[0] === 'Kuma' && ninjas[1] === "Hattori", 'Kuma and Hattori')
+// log(ninjas[2] === undefined, 'But we have lost Yagyu')
 
-log(ninjas.length === 3, 'There are three ninjas')
-log(samurai.length === 2, 'And only two samurai')
-
-log(ninjas[0] === 'Kuma', 'Kuma is the first ninja')
-log(samurai[samurai.length - 1] === 'Tomoe', 'Tomoe is the last samurai')
-
-log(ninjas[4] === undefined, 'We get undefined if we try to access an out of bounds index')
-
-ninjas[4] = 'Ishi'
-log(ninjas.length === 5, 'Arrays are automatically expanded')
-
-ninjas.length = 2
-log(ninjas.length === 2, 'There are only two ninjas now')
-log(ninjas[0] === 'Kuma' && ninjas[1] === "Hattori", 'Kuma and Hattori')
-log(ninjas[2] === undefined, 'But we have lost Yagyu')
+const samurai = new Array('Oda', 'Tomome')
